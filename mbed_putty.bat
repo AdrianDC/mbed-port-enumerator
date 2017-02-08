a7ae5440274f0e6a47d50850b379a46058020743
Adrian DC <AdrianDC@users.noreply.github.com>
2017-02-08 11:47:09 +0100

    mbed_port_enumerator: Putty helper for automated COM binding
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

---
+++ mbed_putty.bat
@@ -0,0 +1,10 @@
+echo off
+cls
+
+set MBED_ENUMERATOR="mbed_port_enumerator.exe"
+set PUTTY="putty.exe"
+
+for /f %%i in ('%MBED_ENUMERATOR%') do set COM_PORT=%%i
+echo Found MBED connected on '%COM_PORT%'
+
+start "" "%PUTTY%" -serial %COM_PORT%
