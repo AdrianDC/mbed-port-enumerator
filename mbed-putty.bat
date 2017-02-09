eb376ad222aa0ab2460b12c5f98e996d501c3923
AdrianDC <radian.dc@gmail.com>
2017-02-09 14:28:10 +0100

    mbed-port-enumerator: Follow general mbed namings
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- mbed-putty.bat
+++ mbed-putty.bat
@@ -1,7 +1,7 @@
 echo off
 cls
 
-set MBED_ENUMERATOR="mbed_port_enumerator.exe"
+set MBED_ENUMERATOR="mbed-port-enumerator.exe"
 set PUTTY="putty.exe"
 
 for /f %%i in ('%MBED_ENUMERATOR%') do set COM_PORT=%%i
