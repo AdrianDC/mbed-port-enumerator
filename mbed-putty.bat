f970bb98337de92cc1411c348249dc9986ee2ffb
AdrianDC <radian.dc@gmail.com>
2017-02-09 14:37:00 +0100

    mbed-port-enumerator: Detect missing Putty.exe file
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- mbed-putty.bat
+++ mbed-putty.bat
@@ -4,6 +4,15 @@ cls
 set MBED_ENUMERATOR="mbed-port-enumerator.exe"
 set PUTTY="putty.exe"
 
+if not exist "%PUTTY%" (
+  echo.
+  echo   ERROR: %PUTTY% executable was not found...
+  echo   Please download the portable Putty.exe and place it in the folder
+  echo.
+  pause
+  exit
+)
+
 for /f %%i in ('%MBED_ENUMERATOR%') do set COM_PORT=%%i
 echo Found MBED connected on '%COM_PORT%'
 
